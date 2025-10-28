814e5519a2e107344249469761d202a51add7f1e
AlejandroVelazquezRuizTi03SM-22 <alejandrovr.ti22@utsjr.edu.mx>
2025-10-28 15:56:33 -0600

    feat(ci): correccion en el archivos de prueba

--- test/golden_test.dart
+++ test/golden_test.dart
@@ -1,4 +1,3 @@
-// test/golden_test.dart
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 import 'package:testing_demo/main.dart';
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,4 +1,3 @@
-// test/widget_test.dart
 import 'package:flutter_test/flutter_test.dart';
 import 'package:testing_demo/main.dart';
 
